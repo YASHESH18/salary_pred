165e9fb76b6c66cf80d2ae96cf1a1b92d8a09880
Yashesh Bhiwapurkar <yasheshofficial05@gmail.com>
2026-04-20 12:10:04 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,7 +9,7 @@ with open('random_forest_model.pkl', 'rb') as file:
     model = pickle.load(file)
 
 # Load the label encoders
-with open('label_encoders.pkl) as file:
+with open('label_encoders.pkl', 'rb') as file:
     label_encoders = pickle.load(file)
 
 # Set page config
